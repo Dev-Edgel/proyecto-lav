1d604f73acb5121f28d01c9ceeaaedd1c428b8fc
Edgar <edgel.infor123@gmail.com>
2019-06-04 18:52:08 -0400

    Mensajes desde el controlador

--- resources/views/includes/mensaje.blade.php
+++ resources/views/includes/mensaje.blade.php
@@ -1,4 +1,3 @@
-    
 @if (session("mensaje"))
 <div class="alert alert-success alert-dismissible" data-auto-dismiss="3000">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
