cdfad6a3083626ca836eda06447c4e12208c8644
Edgar <edgel.infor123@gmail.com>
2019-08-07 19:43:24 -0400

    Sistemas de Permisos Terminado

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['aut
     /*RUTAS DE PERMISO*/
     Route::get('permiso', 'PermisoController@index')->name('permiso');
     Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
+    Route::post('permiso', 'PermisoController@guardar')->name('guardar_permiso');
+    Route::get('permiso/{id}/editar', 'PermisoController@editar')->name('editar_permiso');
+    Route::put('permiso/{id}', 'PermisoController@actualizar')->name('actualizar_permiso');
+    Route::delete('permiso/{id}', 'PermisoController@eliminar')->name('eliminar_permiso');
     /*RUTAS DEL MENU*/
     Route::get('menu', 'MenuController@index')->name('menu');
     Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
@@ -27,4 +31,13 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['aut
     /*RUTAS MENU_ROL*/
     Route::get('menu-rol', 'MenuRolController@index')->name('menu_rol');
     Route::post('menu-rol', 'MenuRolController@guardar')->name('guardar_menu_rol');
+    /*RUTAS PERMISO_ROL*/
+    Route::get('permiso-rol', 'PermisoRolController@index')->name('permiso_rol');
+    Route::post('permiso-rol', 'PermisoRolController@guardar')->name('guardar_permiso_rol');
 });
+Route::get('libro', 'LibroController@index')->name('libro');
+Route::get('libro/crear', 'LibroController@crear')->name('crear_libro');
+Route::post('libro', 'LibroController@guardar')->name('guardar_libro');
+Route::get('libro/{id}/editar', 'LibroController@editar')->name('editar_libro');
+Route::put('libro/{id}', 'LibroController@actualizar')->name('actualizar_libro');
+Route::delete('libro/{id}', 'LibroController@eliminar')->name('eliminar_libro');
