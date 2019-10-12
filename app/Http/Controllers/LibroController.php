a6815a2dafa368ab7404efa1d0ea278f4d43e0c6
Edgar <edgel.infor123@gmail.com>
2019-10-12 13:34:59 -0400

    Subir Archivos con Laravel

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -33,6 +33,7 @@ class LibroController extends Controller
     public function crear()
     {
         can('crear-libros');
+        return view('libro.crear');
     }
 
     /**
@@ -43,7 +44,8 @@ class LibroController extends Controller
      */
     public function guardar(Request $request)
     {
-        //
+        if ($foto = Libro::setCaratula($request->foto_up))
+            $request->request->add(['foto' => $foto]);
     }
 
     /**
